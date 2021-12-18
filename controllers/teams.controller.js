4cea6c466ae91351de96965872eaf5e9f9e920cb
rweebs <67186137+rweebs@users.noreply.github.com>
2021-12-18 19:09:48 +0700

    regist 1 email

--- controllers/teams.controller.js
+++ controllers/teams.controller.js
@@ -19,30 +19,30 @@ dotenv.config();
 module.exports ={
     create: async (req,res)=>{
         const user1= await User.findOne({email:req.email})
-        const user2= await User.findOne({email:req.body.member2_email})
+        // const user2= await User.findOne({email:req.body.member2_email})
         
-        let user3;
-        if(req.body.member3_email){
-            user3= await User.findOne({email:req.body.member3_email})
-            if(!user2 && !user3){
-                return(res.status(404).json({
-                    status: "FAILED",
-                    message: "Member 1's and Member 2s'email have not been registered yet on our website"
-                }))
-            }
-            if(!user3){
-                return(res.status(404).json({
-                    status: "FAILED",
-                    message: "Member 2's email has not been registered yet on our website"
-                }))
-            }
-        }
-        if(!user2){
-            return(res.status(404).json({
-                status: "FAILED",
-                message: "Member 1's email has not been registered yet on our website"
-            }))
-        }
+        // let user3;
+        // if(req.body.member3_email){
+        //     user3= await User.findOne({email:req.body.member3_email})
+        //     if(!user2 && !user3){
+        //         return(res.status(404).json({
+        //             status: "FAILED",
+        //             message: "Member 1's and Member 2s'email have not been registered yet on our website"
+        //         }))
+        //     }
+        //     if(!user3){
+        //         return(res.status(404).json({
+        //             status: "FAILED",
+        //             message: "Member 2's email has not been registered yet on our website"
+        //         }))
+        //     }
+        // }
+        // if(!user2){
+        //     return(res.status(404).json({
+        //         status: "FAILED",
+        //         message: "Member 1's email has not been registered yet on our website"
+        //     }))
+        // }
         const member1= new Member({
             member:user1,
             name:req.body.member1_name,
@@ -53,34 +53,35 @@ module.exports ={
             wa_number:req.body.member1_wa_number,
             post:req.body.member1_post
         })
-        const member2= new Member( {
-            member:user2,
-            name:req.body.member2_name,
-            university_id:req.body.member2_university_id,
-            university_id_link:req.body.member2_university_id_link,
-            id_line:req.body.member2_id_line,
-            wa:req.body.member2_wa,
-            post:req.body.member2_post,
-            wa_number:req.body.member2_wa_number
-        })
-        let member3;
-        if(req.body.member3_email){
-            member3= new Member({
-                member:user3,
-                name:req.body.member3_name,
-                university_id:req.body.member3_university_id,
-                university_id_link:req.body.member3_university_id_link,
-                id_line:req.body.member3_id_line,
-                wa:req.body.member3_wa,
-                wa_number:req.body.member3_wa_number,
-                post:req.body.member3_post
-            })
-        }
+        // const member2= new Member( {
+        //     member:user2,
+        //     name:req.body.member2_name,
+        //     university_id:req.body.member2_university_id,
+        //     university_id_link:req.body.member2_university_id_link,
+        //     id_line:req.body.member2_id_line,
+        //     wa:req.body.member2_wa,
+        //     post:req.body.member2_post,
+        //     wa_number:req.body.member2_wa_number
+        // })
+        // let member3;
+        // if(req.body.member3_email){
+        //     member3= new Member({
+        //         member:user3,
+        //         name:req.body.member3_name,
+        //         university_id:req.body.member3_university_id,
+        //         university_id_link:req.body.member3_university_id_link,
+        //         id_line:req.body.member3_id_line,
+        //         wa:req.body.member3_wa,
+        //         wa_number:req.body.member3_wa_number,
+        //         post:req.body.member3_post
+        //     })
+        // }
         let members;
-        members=[member1,member2]
-        if (req.body.member3_email){
-            members=[member1,member2,member3]
-        }
+        // members=[member1,member2]
+        members=[member1]
+        // if (req.body.member3_email){
+        //     members=[member1,member2,member3]
+        // }
         const team = new Team({
             name: req.body.team_name,
             member:members,
